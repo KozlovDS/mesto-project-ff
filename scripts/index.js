65d86fe2154462cbd2791f9660745ec5c9db3648
Dmitry Kozlov <kozlovv.ds@ya.ru>
2024-01-26 19:57:33 +0300

    my commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function creatCard(cardDetails) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardDetails.link;
+  cardElement.querySelector(".card__title").textContent = cardDetails.name;
 
-// @todo: Функция удаления карточки
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => {
+      cardElement.remove();
+    });
+
+  return cardElement;
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(creatCard(item));
+});
