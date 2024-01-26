1f17359fa1e8bec20c615be04f3771d708b53e7f
Dmitry Kozlov <kozlovv.ds@yandex.ru>
2024-01-26 20:45:35 +0300

    bug fix

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,26 @@ const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
 function creatCard(cardDetails) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").src = cardDetails.link;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = cardDetails.link;
+  cardImage.alt = cardDetails.name;
   cardElement.querySelector(".card__title").textContent = cardDetails.name;
 
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", () => {
-      cardElement.remove();
+      deleteCard(cardElement);
     });
 
   return cardElement;
 }
 
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
-initialCards.forEach((item) => {
+initialCards.forEach(item => {
   placesList.append(creatCard(item));
 });
