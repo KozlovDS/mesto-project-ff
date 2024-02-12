6565baa17966b3fecf6158bc3150ba69158c3d69
Dmitry Kozlov <kozlovv.ds@yandex.ru>
2024-02-12 18:04:10 +0300

    add webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import "../pages/index.css";
+import { initialCards } from "./cards.js";
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
@@ -5,28 +8,26 @@ const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function creatCard(cardDetails) {
+function creatCard(cardDetails, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   cardImage.src = cardDetails.link;
   cardImage.alt = cardDetails.name;
   cardElement.querySelector(".card__title").textContent = cardDetails.name;
 
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", () => {
-      deleteCard(cardElement);
-    });
+  cardElement.querySelector(".card__delete-button").addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
 
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(cardElement) {
+const deleteCard = function (cardElement) {
   cardElement.remove();
-}
+};
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(item => {
-  placesList.append(creatCard(item));
+initialCards.forEach((item) => {
+  placesList.append(creatCard(item, deleteCard));
 });
